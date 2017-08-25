912e61f5a03f774bfc1b46141110c4402fa13147
viviana <viviana.rodriguez.c@outlook.com>
2017-08-24 21:23:07 -0300

    tercer commit

--- SuperHeroes/config/routes.rb
+++ SuperHeroes/config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   get 'pages/batman'
 
+  get 'pages/superman'
+
  root 'pages#batman'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
