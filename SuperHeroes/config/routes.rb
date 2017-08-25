5a01359b4a30f287fbbbb0f113a3e6e3898ff434
viviana <viviana.rodriguez.c@outlook.com>
2017-08-24 22:32:01 -0300

    fin

--- SuperHeroes/config/routes.rb
+++ SuperHeroes/config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
 
   get 'pages/batman-vs-superman'
 
- root 'pages#batman-vs-superman'
- 
+ root 'pages#batman_vs_superman'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
